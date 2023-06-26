4f051c096f56302d73a94d491f39880ddd09d10c
Héctor Martín <hector.martin@vass.es>
2023-06-26 19:00:15 +0200

    Whole refactor of mail php

--- email.php
+++ email.php
@@ -1,40 +1,66 @@
 <?php
 
-$invitado = $_POST['invitado'];
-$acompanante = $_POST['acompanante'];
-$acompananteString = "";
-$viernes = $_POST['viernes'];
-$sabado = $_POST['sabado'];
+// $invitado = $_POST['invitado'];
+// $acompanante = $_POST['acompanante'];
+// $acompananteString = "";
+// $viernes = $_POST['viernes'];
+// $sabado = $_POST['sabado'];
 
-$subject = "Invitado" . $invitado . "acompañante" . $acompanante . "viernes" . $viernes . "sabado" . $sabado;
+// $subject = "Invitado" . $invitado . "acompañante" . $acompanante . "viernes" . $viernes . "sabado" . $sabado;
 
-if (isset($viernes)) {
-  $viernes = ", Viernes";
-} else {
-  $viernes = "";
-}
+// if (isset($viernes)) {
+//   $viernes = ", Viernes";
+// } else {
+//   $viernes = "";
+// }
 
-if (isset($sabado)) {
-  $sabado = ", Sábado";
-} else {
-  $viernes = "";
-}
+// if (isset($sabado)) {
+//   $sabado = ", Sábado";
+// } else {
+//   $viernes = "";
+// }
 
-if (isset($sabado)) {
-  $acompananteString = " acompañado de " . $acompanante;
-} else {
-  $acompananteString = "";
-}
+// if (isset($acompanante)) {
+//   $acompananteString = " acompañado de " . $acompanante;
+// } else {
+//   $acompananteString = "";
+// }
 
 
-$msg = "Hola, soy " . $invitado . " y asistiré " . $viernes . $sabado . $acompanante;
+// $msg = "Hola, soy " . $invitado . " y asistiré " . $viernes . $sxabado . $acompanante;
+
+// $recipient = "Noacrisvic@gmail.com";
+
+// $mailheader = "From:" . $invitado . "<" . $recipient . ">\r\n";
+
+// mail("Noacrisvic@gmail.com", $subject, $msg, $mailheader) or die("Error!");
+
+// echo "email sent!"
+
+
+$invitado = $_POST['invitado'] ?? '';
+$acompanante = $_POST['acompanante'] ?? '';
+$viernes = isset($_POST['viernes']);
+$sabado = isset($_POST['sabado']);
+
+$subject = "Invitado: $invitado, Acompañante: $acompanante, Viernes: $viernes, Sábado: $sabado";
+
+$viernesString = $viernes ? ", Viernes" : "";
+$sabadoString = $sabado ? ", Sábado" : "";
+
+$acompananteString = $acompanante ? " acompañado de $acompanante" : "";
+
+$msg = "Hola, soy $invitado y asistiré $viernesString $sabadoString $acompananteString";
 
 $recipient = "Noacrisvic@gmail.com";
 
-$mailheader = "From:" . $invitado . "<" . $recipient . ">\r\n";
+$mailheader = "From: $invitado <$recipient>\r\n";
 
-mail("Noacrisvic@gmail.com", $subject, $msg, $mailheader) or die("Error!");
+$mailSent = mail("Noacrisvic@gmail.com", $subject, $msg, $mailheader);
 
-echo "email sent!"
-
+if ($mailSent) {
+  echo "¡Correo enviado!";
+} else {
+  echo "¡Error al enviar el correo!";
+}
 ?>
